13907dab1920dcd4bbfb3b90c18fcb7653243eb7
Andrejs Dāvis Umbrovskis <ledaviskylv@gmail.com>
2024-11-06 17:58:47 +0100

    Sign up backend

--- website/app/(auth)/signup/page.tsx
+++ website/app/(auth)/signup/page.tsx
@@ -1,44 +1,11 @@
-"use client";
-import React, { useState } from 'react';
-import { useRouter } from 'next/navigation';
+import React from 'react';
+import SignupForm from './SignupForm';
 
-const Signup: React.FC = () => {
-    const router = useRouter();
-    const [formData, setFormData] = useState({
-        username: '',
-        firstName: '',
-        lastName: '',
-        password: ''
-    });
-
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value });
-    };
-
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-        alert('Account Created Successfully');
-        router.push('/');
-    };
-
+const Signup = () => {
     return (
         <div className="container">
             <h2>Create Account</h2>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="username">Username:</label>
-                <input type="text" id="username" name="username" onChange={handleChange} required />
-
-                <label htmlFor="firstName">First Name:</label>
-                <input type="text" id="firstName" name="firstName" onChange={handleChange} required />
-
-                <label htmlFor="lastName">Last Name:</label>
-                <input type="text" id="lastName" name="lastName" onChange={handleChange} required />
-
-                <label htmlFor="password">Password:</label>
-                <input type="password" id="password" name="password" onChange={handleChange} required />
-
-                <button type="submit">Create Account</button>
-            </form>
+            <SignupForm/>
         </div>
     );
 };
--- website/app/api/actions/auth-actions.ts
+++ website/app/api/actions/auth-actions.ts
@@ -4,11 +4,85 @@ import { connectToDatabase } from '../db-connection';
 import { z } from "zod"
 import { isError } from '@/app/(utils)/utilFuncs';
 
-export async function registerUserAction(formData: FormData): Promise<boolean> {
-    console.log("Hello From Register User Action");
-    return false
+
+/// SIGN UP
+export type signupState = {
+    zodErrors: any,
+    message: string
 }
 
+const signupInputValidator = z.object({
+    username: z.string().min(3).max(30, {
+        message: "Username must be between 3 and 30 characters"
+    }),
+    password: z.string().min(10).max(100, {
+        message: "Password must be between 10 and 100 characters"
+    }),
+    firstName: z.string().min(1, {
+        message: "First name must be at least 1 character"
+    }),
+    lastName: z.string().min(1, {
+        message: "Last name must be at least 1 character"
+    }),
+})
+
+export async function registerUserAction(prevState: signupState, formData: FormData) {
+    const newState: signupState = {
+        zodErrors: null,
+        message: ""
+    }
+    
+    // Validating inputs
+    const validatedFields = signupInputValidator.safeParse({
+        username: formData.get("username"),
+        password: formData.get("password"),
+        firstName: formData.get("firstName"),
+        lastName: formData.get("lastName")
+    })
+
+    // Invalid form input
+    if (!validatedFields.success) {
+        newState.zodErrors = validatedFields.error.flatten().fieldErrors
+        newState.message = "Missing Fields!"
+        console.log(newState)
+
+        return {...prevState, ...newState}
+    }
+
+    // Checking does user exist
+    const dbCon = connectToDatabase()
+    const usernameCheck = await dbCon.usernameExistsAsync(validatedFields.data.username)
+    if (isError(usernameCheck)) {
+        newState.message = usernameCheck.message
+        dbCon.close()
+        return {...prevState, ...newState}
+    }
+    if (usernameCheck === true) {
+        newState.zodErrors = {username: ["Username taken!"]}
+        newState.message = "Registration failed!"
+        dbCon.close()
+        return {...prevState, ...newState}
+    }
+    
+    // Username is free, generating password
+    const encryptedPassword = await encryptPassword(validatedFields.data.password, undefined)
+    // Creating the user
+    dbCon.createAccount(
+        validatedFields.data.username,
+        encryptedPassword.hash,
+        encryptedPassword.salt,
+        validatedFields.data.firstName,
+        validatedFields.data.lastName,
+        undefined
+    )
+    dbCon.close()
+    newState.message = "ok"
+
+    return {...prevState, ...newState}
+}
+
+
+/// AUTHENTICATION
 export type authenticationState = {
     auth: {
         cookie: string,
@@ -52,7 +126,7 @@ export async function authenticateUserAction(prevState: authenticationState, for
         return {...prevState, ...newState}
     }
 
-    // Matching credentials
+    // Checking credentials
     const dbCon = connectToDatabase()
     const usernameCheck = await dbCon.usernameExistsAsync(validatedFields.data.username)
     if (isError(usernameCheck)) {
@@ -101,6 +175,7 @@ export async function authenticateUserAction(prevState: authenticationState, for
     return {...prevState, ...newState}
 }
 
+/// ENCRYPTION
 export async function encryptPassword(password: string, salt: string | undefined): Promise<{hash: Buffer, salt: string}> {
     if (salt === undefined) {
         salt = crypto.randomBytes(16).toString("hex")
