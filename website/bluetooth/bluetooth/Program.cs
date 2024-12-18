ba8b5bd40ef277bacf9d0b29f71f420f7897d666
Andrejs Dāvis Umbrovskis <ledaviskylv@gmail.com>
2024-12-18 20:23:17 +0100

    Bluetooth C# server test

---
+++ website/bluetooth/bluetooth/Program.cs
@@ -0,0 +1,29 @@
+﻿using InTheHand.Bluetooth;
+
+public static class BluetoothConnection
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Scanning for Bluetooth Low Energy (BLE) devices...");
+        try
+        {
+            Console.WriteLine($"Try catch");
+            // Start scanning for devices
+            var bluetoothDevices = await Bluetooth.ScanForDevicesAsync();
+            Console.WriteLine($"devices found: {bluetoothDevices.Count}");
+            foreach (var device in bluetoothDevices)
+            {
+                Console.WriteLine($"Found device:");
+                Console.WriteLine($"  Name: {device.Name ?? "Unknown"}");
+                Console.WriteLine($"  ID: {device.Id}");
+                //Console.WriteLine($"  RSSI: {device.} dBm");
+            }
+
+            Console.WriteLine("Scan completed.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred during scanning: {ex.Message}");
+        }
+    }
+}
